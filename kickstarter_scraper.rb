c259327bac3d10d154ca1a318c10c7e413d4a8bf
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-18 16:24:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,26 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  project_html = kickstarter.css("li.project.grid_4")
+  
+  projects = {}
+  
+  project_html.each do |project|
+    title = project_html.css("h2.bbcard_name strong a").text
+    projects[project] = {}
+  end
+  binding.pry
 end
+
+create_project_hash
+
+#projects = kickstarter.css("li.project.grid_4")
+#title = projects.css("h2.bbcard_name strong a").text
+#img = projects.css("div.project-thumbnail a img").attribute("src").value
+#description = projects.css("p.bbcard_blurb").text
+#location = projects.css("ul.project-meta li a").text
+#funded = projects.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
