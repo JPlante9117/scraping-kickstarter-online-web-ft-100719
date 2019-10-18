0c016ddb0d1830aa15877826eab9bb3c8b8dba30
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-18 16:28:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,10 +12,10 @@ def create_project_hash
   project_html.each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
-      description: project.css("p.bbcard_blurb").text,
-      location: project.css("ul.project-meta li a").text,
-      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i,
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta li a").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i,
     }
   end
   #binding.pry
